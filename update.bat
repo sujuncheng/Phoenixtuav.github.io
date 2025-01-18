d5891460e277a34b7b976f8be2049ddaa395f07e
qllokirin <2456590695@qq.com>
2025-01-18 14:19:14 +0800

    update

--- update.bat
+++ update.bat
@@ -1,6 +1,5 @@
 chcp 65001 
-set HTTP_PROXY=http://127.0.0.1:7890
-set HTTPS_PROXY=https://127.0.0.1:7890
+$env:HTTP_PROXY="http://127.0.0.1:7890"; $env:HTTPS_PROXY="https://127.0.0.1:7890"
 git pull
 git add .
 git commit -m "update"
